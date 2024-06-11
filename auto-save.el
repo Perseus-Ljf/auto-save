222764566a9694162acff76f400f27a45803bb2e
Perseus <psluf9@gmail.com>
2024-06-11 19:07:52 +0800

    fix bugs

--- auto-save.el
+++ auto-save.el
@@ -132,7 +132,7 @@ avoid delete current indent space when you programming."
         (dolist (buf (buffer-list))
           (set-buffer buf)
           (when (and
-                  (or evil-normal-state-p)
+                  (evil-normal-state-p)
                  ;; Buffer associate with a filename?
                  (or (buffer-file-name)
                      (auto-save-is-remote-file))
